7656916b9e306eeee73fe617961076105b79a4de
Limeslices <kohlbyrd7@gmail.com>
2022-12-08 16:29:10 -0600

    added "f*cking"

--- src/swearsJSON.ts
+++ src/swearsJSON.ts
@@ -1,312 +1,316 @@
 const swearsJSON = {
     "RECORDS": [
         {
-        "word": "anal",
-        "language": "en"
-        },
-    {
-        "word": "anus",
-        "language": "en"
-        },
-    {
-        "word": "arse",
-        "language": "en"
-        },
-    {
-        "word": "ass",
-        "language": "en"
-        },
-    {
-        "word": "balls",
-        "language": "en"
-        },
-    {
-        "word": "ballsack",
-        "language": "en"
-        },
-    {
-        "word": "bastard",
-        "language": "en"
-        },
-    {
-        "word": "biatch",
-        "language": "en"
-        },
-    {
-        "word": "bitch",
-        "language": "en"
-        },
-    {
-        "word": "bloody",
-        "language": "en"
-        },
-    {
-        "word": "blow job",
-        "language": "en"
-        },
-    {
-        "word": "blowjob",
-        "language": "en"
-        },
-    {
-        "word": "bollock",
-        "language": "en"
-        },
-    {
-        "word": "bollok",
-        "language": "en"
-        },
-    {
-        "word": "boner",
-        "language": "en"
-        },
-    {
-        "word": "boob",
-        "language": "en"
-        },
-    {
-        "word": "bugger",
-        "language": "en"
-        },
-    {
-        "word": "bum",
-        "language": "en"
-        },
-    {
-        "word": "butt",
-        "language": "en"
-        },
-    {
-        "word": "buttplug",
-        "language": "en"
-        },
-    {
-        "word": "clitoris",
-        "language": "en"
-        },
-    {
-        "word": "cock",
-        "language": "en"
-        },
-    {
-        "word": "coon",
-        "language": "en"
-        },
-    {
-        "word": "crap",
-        "language": "en"
-        },
-    {
-        "word": "cunt",
-        "language": "en"
-        },
-    {
-        "word": "damn",
-        "language": "en"
-        },
-    {
-        "word": "dick",
-        "language": "en"
-        },
-    {
-        "word": "dildo",
-        "language": "en"
-        },
-    {
-        "word": "dyke",
-        "language": "en"
-        },
-    {
-        "word": "f u c k",
-        "language": "en"
-        },
-    {
-        "word": "fag",
-        "language": "en"
-        },
-    {
-        "word": "feck",
-        "language": "en"
-        },
-    {
-        "word": "felching",
-        "language": "en"
-        },
-    {
-        "word": "fellate",
-        "language": "en"
-        },
-    {
-        "word": "fellatio",
-        "language": "en"
-        },
-    {
-        "word": "flange",
-        "language": "en"
-        },
-    {
-        "word": "fuck",
-        "language": "en"
-        },
-    {
-        "word": "fudge packer",
-        "language": "en"
-        },
-    {
-        "word": "fudgepacker",
-        "language": "en"
-        },
-    {
-        "word": "God damn",
-        "language": "en"
-        },
-    {
-        "word": "Goddamn",
-        "language": "en"
-        },
-    {
-        "word": "hell",
-        "language": "en"
-        },
-    {
-        "word": "homo",
-        "language": "en"
-        },
-    {
-        "word": "jerk",
-        "language": "en"
-        },
-    {
-        "word": "jizz",
-        "language": "en"
-        },
-    {
-        "word": "knob end",
-        "language": "en"
-        },
-    {
-        "word": "knobend",
-        "language": "en"
-        },
-    {
-        "word": "labia",
-        "language": "en"
-        },
-    {
-        "word": "lmao",
-        "language": "en"
-        },
-    {
-        "word": "lmfao",
-        "language": "en"
-        },
-    {
-        "word": "muff",
-        "language": "en"
-        },
-    {
-        "word": "nigga",
-        "language": "en"
-        },
-    {
-        "word": "nigger",
-        "language": "en"
-        },
-    {
-        "word": "omg",
-        "language": "en"
-        },
-    {
-        "word": "penis",
-        "language": "en"
-        },
-    {
-        "word": "piss",
-        "language": "en"
-        },
-    {
-        "word": "poop",
-        "language": "en"
-        },
-    {
-        "word": "prick",
-        "language": "en"
-        },
-    {
-        "word": "pube",
-        "language": "en"
-        },
-    {
-        "word": "pussy",
-        "language": "en"
-        },
-    {
-        "word": "queer",
-        "language": "en"
-        },
-    {
-        "word": "s hit",
-        "language": "en"
-        },
-    {
-        "word": "scrotum",
-        "language": "en"
-        },
-    {
-        "word": "sex",
-        "language": "en"
-        },
-    {
-        "word": "sh1t",
-        "language": "en"
-        },
-    {
-        "word": "shit",
-        "language": "en"
-        },
-    {
-        "word": "slut",
-        "language": "en"
-        },
-    {
-        "word": "smegma",
-        "language": "en"
-        },
-    {
-        "word": "spunk",
-        "language": "en"
-        },
-    {
-        "word": "tit",
-        "language": "en"
-        },
-    {
-        "word": "tosser",
-        "language": "en"
-        },
-    {
-        "word": "turd",
-        "language": "en"
-        },
-    {
-        "word": "twat",
-        "language": "en"
-        },
-    {
-        "word": "vagina",
-        "language": "en"
-        },
-    {
-        "word": "wank",
-        "language": "en"
-        },
-    {
-        "word": "whore",
-        "language": "en"
-        },
-    {
-        "word": "wtf",
-        "language": "en"
+            "word": "anal",
+            "language": "en"
+        },
+        {
+            "word": "anus",
+            "language": "en"
+        },
+        {
+            "word": "arse",
+            "language": "en"
+        },
+        {
+            "word": "ass",
+            "language": "en"
+        },
+        {
+            "word": "balls",
+            "language": "en"
+        },
+        {
+            "word": "ballsack",
+            "language": "en"
+        },
+        {
+            "word": "bastard",
+            "language": "en"
+        },
+        {
+            "word": "biatch",
+            "language": "en"
+        },
+        {
+            "word": "bitch",
+            "language": "en"
+        },
+        {
+            "word": "bloody",
+            "language": "en"
+        },
+        {
+            "word": "blow job",
+            "language": "en"
+        },
+        {
+            "word": "blowjob",
+            "language": "en"
+        },
+        {
+            "word": "bollock",
+            "language": "en"
+        },
+        {
+            "word": "bollok",
+            "language": "en"
+        },
+        {
+            "word": "boner",
+            "language": "en"
+        },
+        {
+            "word": "boob",
+            "language": "en"
+        },
+        {
+            "word": "bugger",
+            "language": "en"
+        },
+        {
+            "word": "bum",
+            "language": "en"
+        },
+        {
+            "word": "butt",
+            "language": "en"
+        },
+        {
+            "word": "buttplug",
+            "language": "en"
+        },
+        {
+            "word": "clitoris",
+            "language": "en"
+        },
+        {
+            "word": "cock",
+            "language": "en"
+        },
+        {
+            "word": "coon",
+            "language": "en"
+        },
+        {
+            "word": "crap",
+            "language": "en"
+        },
+        {
+            "word": "cunt",
+            "language": "en"
+        },
+        {
+            "word": "damn",
+            "language": "en"
+        },
+        {
+            "word": "dick",
+            "language": "en"
+        },
+        {
+            "word": "dildo",
+            "language": "en"
+        },
+        {
+            "word": "dyke",
+            "language": "en"
+        },
+        {
+            "word": "f u c k",
+            "language": "en"
+        },
+        {
+            "word": "fag",
+            "language": "en"
+        },
+        {
+            "word": "feck",
+            "language": "en"
+        },
+        {
+            "word": "felching",
+            "language": "en"
+        },
+        {
+            "word": "fellate",
+            "language": "en"
+        },
+        {
+            "word": "fellatio",
+            "language": "en"
+        },
+        {
+            "word": "flange",
+            "language": "en"
+        },
+        {
+            "word": "fuck",
+            "language": "en"
+        },
+        {
+            "word": "fucking",
+            "language": "en"
+        },
+        {
+            "word": "fudge packer",
+            "language": "en"
+        },
+        {
+            "word": "fudgepacker",
+            "language": "en"
+        },
+        {
+            "word": "God damn",
+            "language": "en"
+        },
+        {
+            "word": "Goddamn",
+            "language": "en"
+        },
+        {
+            "word": "hell",
+            "language": "en"
+        },
+        {
+            "word": "homo",
+            "language": "en"
+        },
+        {
+            "word": "jerk",
+            "language": "en"
+        },
+        {
+            "word": "jizz",
+            "language": "en"
+        },
+        {
+            "word": "knob end",
+            "language": "en"
+        },
+        {
+            "word": "knobend",
+            "language": "en"
+        },
+        {
+            "word": "labia",
+            "language": "en"
+        },
+        {
+            "word": "lmao",
+            "language": "en"
+        },
+        {
+            "word": "lmfao",
+            "language": "en"
+        },
+        {
+            "word": "muff",
+            "language": "en"
+        },
+        {
+            "word": "nigga",
+            "language": "en"
+        },
+        {
+            "word": "nigger",
+            "language": "en"
+        },
+        {
+            "word": "omg",
+            "language": "en"
+        },
+        {
+            "word": "penis",
+            "language": "en"
+        },
+        {
+            "word": "piss",
+            "language": "en"
+        },
+        {
+            "word": "poop",
+            "language": "en"
+        },
+        {
+            "word": "prick",
+            "language": "en"
+        },
+        {
+            "word": "pube",
+            "language": "en"
+        },
+        {
+            "word": "pussy",
+            "language": "en"
+        },
+        {
+            "word": "queer",
+            "language": "en"
+        },
+        {
+            "word": "s hit",
+            "language": "en"
+        },
+        {
+            "word": "scrotum",
+            "language": "en"
+        },
+        {
+            "word": "sex",
+            "language": "en"
+        },
+        {
+            "word": "sh1t",
+            "language": "en"
+        },
+        {
+            "word": "shit",
+            "language": "en"
+        },
+        {
+            "word": "slut",
+            "language": "en"
+        },
+        {
+            "word": "smegma",
+            "language": "en"
+        },
+        {
+            "word": "spunk",
+            "language": "en"
+        },
+        {
+            "word": "tit",
+            "language": "en"
+        },
+        {
+            "word": "tosser",
+            "language": "en"
+        },
+        {
+            "word": "turd",
+            "language": "en"
+        },
+        {
+            "word": "twat",
+            "language": "en"
+        },
+        {
+            "word": "vagina",
+            "language": "en"
+        },
+        {
+            "word": "wank",
+            "language": "en"
+        },
+        {
+            "word": "whore",
+            "language": "en"
+        },
+        {
+            "word": "wtf",
+            "language": "en"
         },
     ]
 }
